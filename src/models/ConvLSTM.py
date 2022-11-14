a2a99597aa85476cf84f78944749693aeb2c931c
Jacob Ai-prac Ben S <jldiaz2000@gmail.com>
2022-11-13 22:47:06 -0500

    file restructure 2

---
+++ src/models/ConvLSTM.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+# Create custom dataset class 
+
+# Create train and test split 
+
+# Create data loader
+
+# Implementation of model
+
+# Train/Evaluate functions
+
